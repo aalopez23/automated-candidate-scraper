3584331474d5521dcbe1ed0d217646971660149d
njaurigue <70664938+njaurigue@users.noreply.github.com>
2022-10-06 10:20:52 -0700

    Update spScrape.py
    scroll finished

--- spScrape.py
+++ spScrape.py
@@ -46,14 +46,11 @@ def bot(username, password, query):
     driver.find_element(By.TAG_NAME, 'INPUT').send_keys(Keys.RETURN)
     time.sleep(5)
     print('CLICKING')
-    driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[1]/div[2]/div[3]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[1]/div/div/div/div').click()
-    time.sleep(5)
-    print('SCROLLING')
-    driver.execute_script("window.scrollTo(0, 200)")
-    time.sleep(5)
+    driver.find_element(By.CLASS_NAME, 'od-scrollablePane-content-ItemsScopeList').click()
+    while(True):
+        driver.find_element(By.TAG_NAME, 'html').send_keys(Keys.PAGE_DOWN)
 
     print(len(driver.find_elements(By.CLASS_NAME, 'ms-Link')))
-    time.sleep(200)
 
 
 
