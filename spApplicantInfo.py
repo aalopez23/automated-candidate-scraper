ea7033b22a858ed2effa748b1b311a965ab3c2d0
Steven Tran <97551211+stevendtran@users.noreply.github.com>
2022-10-25 11:20:12 -0700

    Update spApplicantInfo.py

--- spApplicantInfo.py
+++ spApplicantInfo.py
@@ -2,7 +2,7 @@ import spacy #pip install spacy
 import re
 
 name_test1= "Two before narrow not Pamela Toro relied how except moment Dejection assurance led Cilinda Brown certainly So gate at not only nonpoint. Betrayed at properly it of Jonathan Nguyen graceful on. Dinner abroad depart ye turned hearts as wished Therefore would allowance too perfectly gentleman supposing man his now Antonio Lopez Families goodness all eat out bed steepest servants. Explained the incommode sir improving northward immediate eat Man denoting received you sex possible you Shew Park own loud son door less yet"
-
+name_test2= "https://www.linkedin.com/in -antonio- lopez 13a48a212/"
 def name_scrape(doc):
     names = []
     nlp = spacy.load("en_core_web_lg") #pythong -m spacy download en_core_web_lg
@@ -17,9 +17,10 @@ def name_scrape(doc):
         elif ent.label_ == "PERSON":
             print(ent.text)
             names.append(ent.text)
+    print(names)
     return names
             
-name_scrape(name_test1)
+name_scrape(name_test2)
 
 
 email_test1 = "Please contact us at contact@tutorialspoint.com for further information. jsn006@ucsd.edu and I also want to test this one: 36363@fjd"
