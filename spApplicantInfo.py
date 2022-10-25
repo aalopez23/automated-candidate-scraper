678597c70ee84e4e8573580b46ff9e4cc0190160
Steven Tran <97551211+stevendtran@users.noreply.github.com>
2022-10-25 10:34:19 -0700

    small fixes

--- spApplicantInfo.py
+++ spApplicantInfo.py
@@ -1,10 +1,11 @@
 import spacy #pip install spacy
+import re
 
-test1= "Two before narrow not relied how except moment Dejection assurance led certainly So gate at not only nonpoint. Betrayed at properly it of Jonathan Nguyen graceful on. Dinner abroad depart ye turned hearts as wished Therefore would allowance too perfectly gentleman supposing man his now Antonio Lopez Families goodness all eat out bed steepest servants. Explained the incommode sir improving northward immediate eat Man denoting received you sex possible you Shew Park own loud son door less yet"
+name_test1= "Two before narrow not relied how except moment Dejection assurance led certainly So gate at not only nonpoint. Betrayed at properly it of Jonathan Nguyen graceful on. Dinner abroad depart ye turned hearts as wished Therefore would allowance too perfectly gentleman supposing man his now Antonio Lopez Families goodness all eat out bed steepest servants. Explained the incommode sir improving northward immediate eat Man denoting received you sex possible you Shew Park own loud son door less yet"
 
-def name_scrape(test):
+def name_scrape(doc):
     nlp = spacy.load("en_core_web_lg") #pythong -m spacy download en_core_web_lg
-    doc = nlp(test)
+    doc = nlp(doc)
     for ent in doc.ents:
         if ent.label_ == "PERSON":
             print(ent.text, ent.label_)
@@ -15,14 +16,12 @@ def name_scrape(test):
             ## Don't do anything.
              print("Picked up Cilinda's name.")
             
-name_scrape(test1)
+name_scrape(name_test1)
 
 
-### [Potential] Extract emails from text
-""" import re
-text = "Please contact us at contact@tutorialspoint.com for further information."+\
-        " You can also give feedbacl at feedback@tp.com"
-
-
-emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", text)
-print emails """
+email_test1 = "Please contact us at contact@tutorialspoint.com for further information."
+def email_scrape(doc):
+    emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", doc)
+    print (emails)
+    
+email_scrape(email_test1)
