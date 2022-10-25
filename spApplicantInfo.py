0bef965bf9002ecc00a2b81b7e26a6d5e1a3113a
Steven Tran <97551211+stevendtran@users.noreply.github.com>
2022-10-25 10:39:39 -0700

    Update spApplicantInfo.py

--- spApplicantInfo.py
+++ spApplicantInfo.py
@@ -1,7 +1,7 @@
 import spacy #pip install spacy
 import re
 
-name_test1= "Two before narrow not relied how except moment Dejection assurance led certainly So gate at not only nonpoint. Betrayed at properly it of Jonathan Nguyen graceful on. Dinner abroad depart ye turned hearts as wished Therefore would allowance too perfectly gentleman supposing man his now Antonio Lopez Families goodness all eat out bed steepest servants. Explained the incommode sir improving northward immediate eat Man denoting received you sex possible you Shew Park own loud son door less yet"
+name_test1= "Two before narrow not relied how except moment Dejection Cilinda Brown assurance led certainly So gate at not only nonpoint. Betrayed at properly it of Jonathan Nguyen graceful on. Dinner abroad depart ye turned hearts as wished Therefore would allowance too perfectly gentleman supposing man his now Antonio Lopez Families goodness all eat out bed steepest servants. Explained the incommode sir improving northward immediate eat Man denoting received you sex possible you Shew Park own loud son door less yet"
 
 def name_scrape(doc):
     nlp = spacy.load("en_core_web_lg") #pythong -m spacy download en_core_web_lg
@@ -22,6 +22,6 @@ name_scrape(name_test1)
 email_test1 = "Please contact us at contact@tutorialspoint.com for further information."
 def email_scrape(doc):
     emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", doc)
-    print (emails)
-    
+    print (emails[0])
+       
 email_scrape(email_test1)
