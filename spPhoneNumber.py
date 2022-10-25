d8c91e11e7299d9c84327670783cff5a59182f25
Sanh Nguyen <92162587+NguyenSanh@users.noreply.github.com>
2022-10-25 11:56:04 -0700

    Added Regex Instructions/Rules
    Also, commented out the previous function from phonenumbers library.

--- spPhoneNumber.py
+++ spPhoneNumber.py
@@ -6,7 +6,7 @@ phone_example = "3774728288"  # U.S phone
 text = "Hi Jane, thank you for reaching out to learn more about Data Science Sphere! The best way to learn more about us is to call us. Feel free to call us at 020 8366 1177 or at 020 8785 7465. There will be no obligation to sign up to our newsletter after the call! Best regards."
 
 
-def phone_scrape(text):
+""" def phone_scrape(text):
     parsed_number = pn.parse(phone_example, "US") # Returns U.S country code (+1) and phone number
     print(parsed_number)
 
@@ -17,13 +17,20 @@ def phone_scrape(text):
         
 
 ### Run 
-""" phone_num1 = str(phone_scrape(phone_example))
-phone_num2 = str(phone_scrape(text)) """
+phone_num1 = str(phone_scrape(phone_example))
+phone_num2 = str(phone_scrape(text))
 
-print(phone_scrape(phone_example))
+print(phone_scrape(phone_example))  """
 
-def find_numbers(doc):
+### Basically, the regular expression (regex) lays out these rules:
+### 1.) The matched string may start with '+' or '(' symbol.
+### 2.) It has to be followed by a number between 1-9.
+### 3.) It has to end with a number between 0-9.
+### 4.) It may contain 0-9 (space) .-() in the middle.
+
+def phone_scrape(doc):
     numbers = re.findall(r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]', doc)
     print(numbers)
     
-find_numbers(text)
+phone_scrape(text)
+phone_scrape(phone_example)
