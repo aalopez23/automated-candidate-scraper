b3ad8ad41a61f38a5b0760fe6bfd5b4a5b8fc754
Sanh Nguyen <92162587+NguyenSanh@users.noreply.github.com>
2022-10-25 14:18:48 -0700

    Restructured Project
    Added separate folder for word document resumes to test on. Tested doc converter on two docs and saved into directory.

--- docConverter.py
+++ docConverter.py
@@ -9,5 +9,5 @@ import aspose.words as aw # pip install aspose-words
 # Takes a Word Document passed in
 # Converts into .txt file as desired name
 # Saves txt file into same directory 
-doc = aw.Document("Matthew_Williams.docx")
-doc.save("Output.txt")
+doc = aw.Document("ResumeDocs\Andrew_Chang.docx")
+doc.save("AndrewResume.txt")
