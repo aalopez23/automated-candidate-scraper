f11768500e5276b8d8304a2c7cd7f52c6ac954d9
Sanh Nguyen <92162587+NguyenSanh@users.noreply.github.com>
2022-10-25 11:13:45 -0700

    Update spPhoneNumber.py

--- spPhoneNumber.py
+++ spPhoneNumber.py
@@ -1,13 +1,22 @@
 import phonenumbers as pn
 
-phone_number_string = "(209) 921-6581"
-parsed_number = pn.parse(phone_number_string, "US")
-
-print(parsed_number)
+# Test Strings
+#phone_example = "(209) 921-6581"  # U.S phone 
 
 text = "Hi Jane, thank you for reaching out to learn more about Data Science Sphere! The best way to learn more about us is to call us. Feel free to call us at 020 8366 1177 or at 020 8785 7465. There will be no obligation to sign up to our newsletter after the call! Best regards."
 
-numbers = pn.PhoneNumberMatcher(text, "GB")
 
-for number in numbers:
-    print(number)
+def phone_scrape(text):
+    #parsed_number = pn.parse(phone_example, "US") # Returns U.S country code (+1) and phone number
+    #print(parsed_number)
+
+
+    numbers = pn.PhoneNumberMatcher(text, "GB")
+    
+    for number in numbers:
+        print(number)
+        
+
+### Run 
+#phone_scrape(phone_example)
+phone_scrape(text)
