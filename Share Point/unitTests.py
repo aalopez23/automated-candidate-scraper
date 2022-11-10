b076f92bbabed4232ad8d37f1806d4dc52091e39
Samuel Liu <sll002@ucsd.edu>
2022-11-09 16:00:37 -0800

    created unitTests rig

---
+++ Share Point/unitTests.py
@@ -0,0 +1,40 @@
+# Author: Samuel Liu
+# Package: spScrape, Share Point Scraper
+# References: spApplicantInfo.py, spScrape.py, Paste.py
+# Description: testing file made to test individual scraping methods on edge cases
+# of resumes that might break the system.
+# 
+# There is a test_doc.txt that you can use to dump resume text into 
+# for single case testing
+
+from spApplicantInfo import *
+import os
+import time
+import pyautogui
+import spacy
+from Paste import *
+import en_core_web_lg
+
+def nameScrape_test():
+    #open the test doc first
+    path = 'test_doc.txt'
+    path = os.path.realpath(path)
+    #os.startfile(path)
+
+    #read the file into the test rig
+    try:
+        with open('test_doc.txt', 'r') as f:
+            text = f.read()
+    except:
+        text = 'ERROR'
+    
+    #call the scraper
+    names = name_scrape(text)
+    return names
+
+def main():
+    nameScrape_test()
+    print("Finished tests.")
+
+if __name__ == "__main__":
+    main()
